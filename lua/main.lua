a05137f75c88b60f353ca136fc312a92a4aadde1
Boris Reuderink <b.reuderink@gmail.com>
2013-01-18 14:09:13 +0100

    Clean up the Lua example a bit more.

--- lua/main.lua
+++ lua/main.lua
@@ -4,8 +4,6 @@ mindplay = require 'mindplay'
 
 -- Add some helper functions that Lua seems to be missing. --
 function isnan(n) return tostring(n) == tostring(0/0) end
-
--- Lua doesn't have a native sleep?
 function sleep(secs)
   t = os.clock() + secs
   while(os.clock() < t) do
@@ -13,13 +11,9 @@ function sleep(secs)
   end
 end
 
---
+-- Define some MindPlay related functions.
 local RESPONSE_READY = 2
 local RESPONSE_INVALID = 3
-local FPS = 30
-local user_id ='test_user'
-local stream_id = 'test_stream'
-
 
 local function cleanup_transfers(list)
   for i, r in ipairs(list) do
@@ -31,35 +25,32 @@ local function cleanup_transfers(list)
   end
 end
 
--- Initialize API:
+-- Initialize API and setup bookkeeping.
 local mp = mindplay.init('localhost:5000')
+
+local user_id ='test_user'
+local stream_id = 'test_stream'
 local detections = {}
 local annotations = {}
 
 -- Start main event loop.
-for i=0,10000 do
-  print('Frame ' .. i .. ', ' .. 
-    #detections + #annotations.. ' responses in transfer.')
+local FPS = 30
+local frame = 0
+while true do
+  frame = frame + 1
+  io.write('.'); io.flush()  -- Show progress.
 
-  -- Regularly request a new prediction from the server:
-  if (i % math.floor(FPS/8)) == 0 then
+  -- Regularly request a new prediction from the server. --
+  if (frame % math.floor(FPS/8)) == 0 then
     -- Perform (non-blocking) request for a detection:
     r = mindplay.request_detection(mp, user_id, stream_id)
-    if r then
-      table.insert(detections, r)
-    else
-      print('Cannot perform request!')
-    end
+    if r then table.insert(detections, r) end
   end
-  if i % 20 == 0 then
-    -- Send annotation with the current frame.
-    msg = 'frame ' .. i
-    r = mindplay.annotate(mp, user_id, stream_id, 'lua_example', msg)
-    if r then
-      table.insert(annotations, r)
-    else
-      print('Cannot perform request!')
-    end
+
+  if frame % 100 == 0 then
+    -- Send annotation.
+    r = mindplay.annotate(mp, user_id, stream_id, 'lua_example', 'Hi!')
+    if r then table.insert(annotations, r) end
   end
 
   -- Update asynchronous transfers in progress:
@@ -67,18 +58,16 @@ for i=0,10000 do
 
   -- Loop over and handle responses:
   for i, r in ipairs(detections) do
-    print(r)
     if mindplay.response_status(r) == RESPONSE_READY then
-      p = mindplay.detection(r, 'random')
-      print('p = ' .. p)
+      print('p = ' .. string.format('%.2f', mindplay.detection(r, 'random')))
     end
   end
 
-  cleanup_transfers(detections)
+  cleanup_transfers(detections); 
   cleanup_transfers(annotations)
 
   sleep(1/FPS)
 end
 
--- Cleanup again.
+-- Cleanup API.
 mindplay.destroy(mp)
