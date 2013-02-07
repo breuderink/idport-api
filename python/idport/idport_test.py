a55f181c447cdfe4acc26fe53c1b5c3e0e0f6407
Boris Reuderink <b.reuderink@gmail.com>
2013-02-07 16:21:17 +0100

    Improve mocking for post-samples test.

--- python/idport/idport_test.py
+++ python/idport/idport_test.py
@@ -3,24 +3,25 @@ import requests
 import idport
 
 def test_post_stream():
-  '''Test the intended code path for posting a stream.'''
-  with mock.patch('requests.post') as rpost:
+  '''Test the intended (nice) code path for posting a stream.'''
+  with mock.patch('requests.post') as mock_post:
     # Create a mocked response object.
-    response = mock.MagicMock()
-    response.raise_for_status = mock.MagicMock()
-    response.json = mock.MagicMock(return_value=dict(stream_id='test_stream'))
+    mock_r = mock.create_autospec(requests.Response)
+    mock_r.json = mock.MagicMock(return_value=dict(stream_id='test_stream'))
 
     # Make request.post return the response object.
-    rpost.return_value = response
+    mock_post.return_value = mock_r
 
     # Do the actual call.
     labels, fs, hw_id = ['a', 'b', 'c'], 64, 'test_hw'
     stream_id = idport.post_stream(
       'http://example.com', 'test-user', labels, fs, hw_id)
 
-    # Verify calls and result.
-    rpost.assert_called_with('http://example.com/u/test-user/s', 
+    # Verify URL and data posted to URL.
+    mock_post.assert_called_with('http://example.com/u/test-user/s', 
       data='{"hardware_id": "test_hw", "sample_rate": 64.0, ' +
         '"sensor_labels": ["a", "b", "c"]}')
+    # Test that errors are raised.
+    mock_r.raise_for_status.assert_called_with()
 
     assert stream_id == 'test_stream'
