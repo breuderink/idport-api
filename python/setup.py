0e1630b6e89b7cf6c11e145dcae42e86c11b0b43
Boris Reuderink <b.reuderink@gmail.com>
2013-02-07 14:09:26 +0100

    First attempt at creating a Python package for IDport.

---
+++ python/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='IDport',
+    version='0.1.0',
+    author='Boris Reuderink',
+    author_email='boris@senzing.com',
+    packages=['idport'],
+    scripts=[],
+    url='https://github.com/breuderink/idport-api',
+    license='LICENSE.txt',
+    description='Python API for IDport brain-computer interface server',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'numpy',
+        'requests >= 1.1.0',
+    ],
+)
