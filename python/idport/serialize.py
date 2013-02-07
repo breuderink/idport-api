eae5b4b9f9e04f360a44415ebcaab1dcd589f14c
Boris Reuderink <b.reuderink@gmail.com>
2013-02-07 20:33:50 +0100

    Add doctest for deserialization of JSON encoded samples.

--- python/idport/serialize.py
+++ python/idport/serialize.py
@@ -22,7 +22,8 @@ def deserialize_singles(string):
   r'''
   Example
   -------
-  >>> deserialize_singles('AAAAAAAAgD8AAABA2w9JQA==\n') # doctest: +NORMALIZE_WHITESPACE
+  >>> deserialize_singles('AAAAAAAAgD8AAABA2w9JQA==\n')
+  ... # doctest: +NORMALIZE_WHITESPACE
   array([ 0. ,  1. ,  2. , 3.14159274], dtype=float32)
   '''
   return np.fromstring(string.decode('base64'), '<f4')
@@ -52,3 +53,43 @@ def serialize_samples(samples, local_time, server_time=None):
     d['server_time'] = server_time
 
   return json.dumps(d, indent=2)
+
+
+def deserialize_samples(s):
+  r'''
+  Deserialize samples from JSON format.
+
+  Returns
+  -------
+  samples : NumPy array with shape (n, p)
+    Two dimensional array with n samples from p sensors, stored in
+    single precision floating point format.
+  local_time : float
+    Client timestamp of moment that the samples were recorded.
+  server_time : float
+    Server timestamp of moment that the samples were received.
+
+  Example
+  -------
+  First we serialize some samples:
+  >>> S0 = [[0, 1, 2, 3], [1, 1, 2, 3]]
+  >>> payload = serialize_samples(S0, 123.)
+  >>> type(payload)
+  <type 'str'>
+    
+  Now, we can extract the information again:
+  >>> S, ltime, stime = deserialize_samples(payload)
+  >>> S
+  array([[ 0.,  1.,  2.,  3.],
+         [ 1.,  1.,  2.,  3.]], dtype=float32)
+  >>> ltime
+  123.0
+  >>> stime
+  nan
+  '''
+  d = json.loads(s)
+  samples = np.asarray([deserialize_singles(samp) for samp in d['samples']])
+  ltime = float(d.get('local_time', 'nan'))
+  stime = float(d.get('server_time', 'nan'))
+
+  return samples, ltime, stime
